ab547a29dc7b545d44e76ba68c894430122cf1c9
Oscar Harron <oscarharron@hotmail.com>
2022-12-02 16:08:23 +1300

    add colours

--- components/Projects/Container.js
+++ components/Projects/Container.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 import ProjectImage from './ProjectImage'
+import ProjectName from './ProjectName'
 import styles from '../../styles/Projects.module.css'
-import { useInView } from 'react-hook-inview'
 
 
 export default function Container({proj}) {
-  const [ref, isVisible] = useInView()
 
   
 
   return (
-    <div ref={ref} className={styles.container}>
-      <ProjectCard isVisible={isVisible} proj={proj} />
+    <div className={styles.container}>
+      <ProjectCard  proj={proj} />
       <div className={styles.imageCard}>
-        <div className={styles.name}>
-        <h3>{proj.name}</h3>
-
-        </div>
-        <ProjectImage isVisible={isVisible} img={proj.image} />
+        <ProjectName name={proj.name}/>
+        <ProjectImage  img={proj.image} />
       </div>
 
     </div>
