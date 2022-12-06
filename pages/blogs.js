fe917f77130506725ce53d38b7bafa8e8c7fc6a7
Oscar Harron <oscarharron@hotmail.com>
2022-12-06 17:10:03 +1300

    updated all blogs styling

--- pages/blogs.js
+++ pages/blogs.js
@@ -3,6 +3,8 @@ import Head from "next/head"
 import Link from "next/link"
 import fs from 'fs'
 import matter from "gray-matter"
+import styles from '../styles/Posts.module.css'
+import Navbar from "../components/Navbar/Navbar"
 
 export const getStaticProps = async () => {
   const files = fs.readdirSync('posts')
@@ -16,7 +18,6 @@ export const getStaticProps = async () => {
       frontmatter
     }
   })
-  console.log(posts);
   return {
     props: {
       posts
@@ -26,10 +27,24 @@ export const getStaticProps = async () => {
 
 
 export default function Blogs({posts}) {
-  console.log(posts);
+
   return (
-    <div className="main">
-      <h1>hi</h1>
+    <div className={styles.container}>
+       <Head>
+        <title>Oscar Harron</title>
+        <meta name="description" content="Its me" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,700&display=swap" rel="stylesheet" />
+      </Head>
+      <Navbar />
+      <main className={styles.main}>
+        <h1>Posts</h1>
+        <div className={styles.postsWrapper}>
+
+
       {posts.map((blogPost, i) => (
         <div key={i}>
           <Link href={`/posts/${blogPost.slug}`}>
@@ -37,8 +52,9 @@ export default function Blogs({posts}) {
           </Link>
           </div>
       ))}
+              </div>
 
-      
+</main>
    
     </div>
   )
