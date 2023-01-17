808f0c1f9b99493a593257afa0c62414d6a99374
Oscar Harron <oscarharron@hotmail.com>
2023-01-17 16:26:05 +1300

    init about section"

---
+++ components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../../styles/About.module.css'
+
+export default function About() {
+  return (
+   <section>
+    <h2>
+      <span>01</span>ABOUT
+    </h2>
+    <div className={styles.wrapper}>
+      <h3>Hello world</h3>
+    </div>
+    </section>
+  )
+}
