fd6098c93e55651b5a0e69055ec706c172482473
Oscar Harron <oscarharron@hotmail.com>
2023-01-21 15:18:01 +1300

    add remake page

---
+++ pages/remake.js
@@ -0,0 +1,46 @@
+import Head from "next/head";
+
+
+export default function Remake() {
+  return (
+    <div className={'container'}>
+      <Head>
+        <title>Oscar Harron | Blogs</title>
+        <meta
+          name="Here you will find my blog posts on Web Development and beyond."
+          content="Its me"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+      </Head>
+
+      <main className={'main'}>
+        <div>TESTING</div>
+      </main>
+    </div>
+  );
+}
