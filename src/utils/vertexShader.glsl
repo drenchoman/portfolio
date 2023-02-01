0648f8b017dadccff38515eb4788e94c8dc04de3
Oscar Harron <oscarharron@hotmail.com>
2023-02-01 16:07:46 +1300

    update light position

--- src/utils/vertexShader.glsl
+++ src/utils/vertexShader.glsl
@@ -9,7 +9,7 @@ void main(void) {
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   // General calculations needed for diffuse lighting
   // Calculate a vector from the fragment location to the light source
-  vec3 surfaceToLightDirection = vec3( modelViewMatrix * vec4(position, 1.0));
-  vec3 worldLightPos = vec3( viewMatrix * vec4(uLightPos, 1.0));
+  vec3 surfaceToLightDirection = vec3( modelViewMatrix * vec4(position, 1));
+  vec3 worldLightPos = vec3( viewMatrix * vec4(uLightPos, 1.15));
   vSurfaceToLight = normalize(worldLightPos - surfaceToLightDirection);
 }
